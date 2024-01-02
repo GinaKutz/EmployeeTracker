c8c85c0bb3bb475f094b89d257124a129466f805
Preston Fassbender <fassbenderp0551@gmail.com>
2024-01-02 17:28:35 -0600

    week 6 day 3 solved

--- 06-Server-Side-APIs/01-Activities/27-Stu_Mini-Project/Main/assets/js/script.js
+++ 06-Server-Side-APIs/01-Activities/27-Stu_Mini-Project/Main/assets/js/script.js
@@ -1,17 +1,19 @@
 var searchFormEl = document.querySelector('#search-form');
 
 function handleSearchFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var searchInputVal = document.querySelector('#search-input').value;
-    var formatInputVal = document.querySelector('#format-input').value;
+  var searchInputVal = document.querySelector('#search-input').value;
+  var formatInputVal = document.querySelector('#format-input').value;
 
-    if (!searchInputVal) {
-        console.error('You need a search input value!');
-        return;
-    }
+  if (!searchInputVal) {
+    console.error('You need a search input value!');
+    return;
+  }
 
-    //Set the search params in the URL (i.e. `?q=london&format=photo`)
+  var queryString = './search-results.html?q=' + searchInputVal + '&format=' + formatInputVal;
+
+  location.assign(queryString);
 }
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
